75f3ba5dc779541217794d18b8a10bcea91b9dbe
danialrohaizat012-sys <danialrohaizat012@gmail.com>
2026-08-28 16:33:55 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='uruz-flow-v4';const A=['./','index.html','manifest.json','uruz-logo.png','icon-192.png','icon-512.png'];
+const C='uruz-flow-v5';const A=['./','index.html','manifest.json','uruz-logo.png','icon-192.png','icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
