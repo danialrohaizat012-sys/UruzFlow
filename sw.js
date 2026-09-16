f2949b9bb3fa2e93be94e3b3a1199eb3a0e14268
danialrohaizat012-sys <danialrohaizat012@gmail.com>
2026-09-16 20:41:32 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='uruz-flow-v16-2-intake-fix';
+const C='uruz-flow-v16-3-intake-repair';
 const A=['./','index.html','manifest.json','uruz-logo.png','icon-192.png','icon-512.png','apple-touch-icon.png','favicon.svg','favicon-16.png','favicon-32.png','favicon-48.png','favicon-64.png','uruz-emblem-original.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
